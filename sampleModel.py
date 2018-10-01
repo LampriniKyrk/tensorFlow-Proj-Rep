1b2a30feb8f91fa023cfd90d0d3061317fbbb91e
LampriniKyrk <42893108+LampriniKyrk@users.noreply.github.com>
2018-10-01 18:10:21 +0300

    running version

--- sampleModel.py
+++ sampleModel.py
@@ -1,9 +1,14 @@
 import cPickle, sys
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers.convolutional import Convolution2D
+from keras.layers.core import Flatten, Dense, Activation
+from keras.utils import np_utils
+
 
 #------------UNPICKLE-------------#
-f = open('microwave1_PAA_64_GADF 0.pkl', 'rb')
+f = open('fridge/fridge1_PAA_64_GADF 0.pkl', 'rb')
 pickledFile = cPickle.load(f)
 #pickledFile = f.readlines()
 print pickledFile
@@ -27,11 +32,27 @@ print temp_X.shape , temp_Y.shape
 #that should be a tensor
 
 #use conv2d
+n_images = 1
+train_X = np.reshape(temp_X, (n_images, 64, 64, 1))
+#train_Y = np.reshape(temp_Y,(7200))
 
-train_X = np.reshape(temp_X, (7200, 7200, 1))
-train_Y = np.reshape(temp_Y,(7200))
+print train_X.shape
+#print train_Y
 
-print train_Y.shape
-print train_Y
+#try conv2d
+#Create model
+model = Sequential()
+model.add(Convolution2D(32, kernel_size=(3, 3), activation='relu', input_shape=(64,64,1)))
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('softmax'))
 
-#try conv2d 
+model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+model.fit(train_X,temp_Y, batch_size= 1)
+
+
+
+
